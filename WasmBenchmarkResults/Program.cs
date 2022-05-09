ff6b3c615ac6eca3d757e36483caa0b6a90af78b
Radek Doulik <radekdoulik@gmail.com>
2022-05-09 23:00:33 +0200

    Initial import

---
+++ WasmBenchmarkResults/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WasmBenchmarkResults
+{
+    public class Program
+    {
+        SortedDictionary<DateTimeOffset, ResultsData> timedPaths = new();
+
+        static public int Main()
+        {
+            new Program().Run();
+
+            return 0;
+        }
+
+        void Run()
+        {
+            FindResults(".");
+            ExportCSV("results.csv");
+        }
+
+        void FindResults(string path)
+        {
+            foreach (var dir in Directory.GetDirectories(path))
+            {
+                if (!File.Exists(Path.Combine(dir, "git-log.txt")) || !File.Exists(Path.Combine(dir, "results.json")))
+                    continue;
+
+                var data = new ResultsData(dir);
+                timedPaths[data.commitTime] = data;
+            }
+        }
+
+        public void ExportCSV(string path)
+        {
+            using (var sw = new StreamWriter(path))
+            {
+                SortedSet<string> labels = new();
+                foreach (var pair in timedPaths)
+                {
+                    Console.WriteLine($"date: {pair.Key} path: {pair.Value.runPath}");
+                    labels.UnionWith(pair.Value.MeasurementLabels);
+                }
+
+                foreach (var l in labels)
+                    Console.WriteLine($"l: {l}");
+
+                sw.Write($"Task - Measurement");
+                foreach (var d in timedPaths.Keys)
+                {
+                    sw.Write($",{d.Date.ToShortDateString()}");
+                }
+
+                sw.WriteLine();
+
+                foreach (var l in labels)
+                {
+                    sw.Write($"{l.Replace(",", " -")}");
+
+                    foreach (var p in timedPaths)
+                    {
+                        var mt = p.Value.results.minTimes;
+                        var v = mt.ContainsKey(l) ? mt[l].ToString() : "N/A";
+                        sw.Write($",{v}");
+                    }
+
+                    sw.WriteLine();
+                }
+
+                sw.Close();
+            }
+        }
+    }
+}
---
+++ WasmBenchmarkResults/ResultsData.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace WasmBenchmarkResults
+{
+    class JsonResultsData
+    {
+        public List<BenchTask.Result> results;
+        public Dictionary<string, double> minTimes;
+        public DateTime timeStamp;
+
+        public static JsonResultsData? Load(string path)
+        {
+            var options = new JsonSerializerOptions { IncludeFields = true };
+            return JsonSerializer.Deserialize<JsonResultsData>(File.ReadAllText(path), options);
+        }
+    }
+
+    internal class ResultsData
+    {
+        public string runPath;
+        public DateTimeOffset commitTime;
+        public JsonResultsData results;
+
+        public ResultsData(string path)
+        {
+            runPath = path;
+            results = JsonResultsData.Load(Path.Combine(path, "results.json"));
+            commitTime = LoadGitLog(Path.Combine(path, "git-log.txt"));
+        }
+
+        public DateTimeOffset LoadGitLog(string path)
+        {
+            var lines = File.ReadAllLines(path);
+            var regex = new Regex(@"^Date: +(.*)$");
+            string? dateString = null;
+            foreach (var line in lines)
+            {
+                var match = regex.Match(line);
+                if (!match.Success)
+                    continue;
+
+                dateString = match.Groups[1].Value;
+
+                if (!DateTimeOffset.TryParseExact(dateString, "ddd MMM d HH:mm:ss yyyy K", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out var date))
+                    continue;
+
+                return date;
+            }
+
+            throw new InvalidDataException("unable to load git log data");
+        }
+
+        public HashSet<string> MeasurementLabels => results.minTimes.Keys.ToHashSet<string>();
+    }
+}
+
