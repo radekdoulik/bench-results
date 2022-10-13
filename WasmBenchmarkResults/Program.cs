7b83d466c3a233e6e86f389b1253913a4595e916
Radek Doulik <radekdoulik@gmail.com>
2022-10-13 14:42:38 +0200

    Remove unnecessary using clauses

--- WasmBenchmarkResults/Program.cs
+++ WasmBenchmarkResults/Program.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
+﻿using System.IO.Compression;
 using System.Text;
 using System.Text.Json;
-using System.Text.RegularExpressions;
 
 namespace WasmBenchmarkResults
 {
