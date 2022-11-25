776e671d4498a0f81ed8832d17024a0be244727b
Radek Doulik <radek.doulik@gmail.com>
2022-11-25 18:24:31 +0100

    Few tweaks to remeasure

--- scripts/remeasure.sh
+++ scripts/remeasure.sh
@@ -1,15 +1,16 @@
 killall HttpServer
 killall firefox
 killall chrome
+sleep 2
 killall -9 HttpServer
 killall -9 firefox
 killall -9 chrome
 
-DOTNET_ROOT=~/dotnet ~/simple-server/bin/Release/net6.0/HttpServer > server.log
-sleep 2
+DOTNET_ROOT=~/dotnet ~/simple-server/bin/Release/net6.0/HttpServer > server.log &
+sleep 5
 BENCH_URL=`sed -e 's/Listening on //' < server.log`
 echo Url: $BENCH_URL${url_suffix}
-if [ "`pwd`" == *"firefox"* ]
+if [[ "`pwd`" == *"firefox"* ]]
 then
    BROWSER=firefox
 else
